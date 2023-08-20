feff2a2453d351567b9cc8fe11bf2463996c0039
Saeid Zolfaghari <saeidz70@gmail.com>
2023-08-20 18:23:35 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+from sensors.passenger_IN_sens import PassengerInSensor
+from sensors.passenger_OUT_sens import PassengerOutSensor
+from sensors.humidity_sens import HumiditySensor
+
+
+if __name__ == '__main__':
+
+    while True:
+        # Create an instance of the Sensors class
+
+        passenger_in = PassengerInSensor().passenger_counter()
+        # publish passenger_in TODO
+
+        passenger_out = PassengerOutSensor().passenger_counter()
+        # publish passenger_out TODO
+
+        humidity = HumiditySensor().humiditySens()
+        # publish humidity TODO
+
+        # Wait for 1 second before generating the next sensor data
+        time.sleep(1)
---
+++ sensors/humidity_sens.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+
+class HumiditySensor:
+    def __init__(self, mean=40, deviation=3):
+        self.mean = mean
+        self.deviation = deviation
+        self.previous_humidity = None
+
+    def humiditySens(self):
+        if self.previous_humidity is None:
+            humidity_sens = random.gauss(self.mean, self.deviation)
+        else:
+            humidity_sens = random.gauss(
+                (self.previous_humidity + self.mean) / 2,
+                self.deviation / 2
+            )
+
+        humidity_sens = max(0, min(100, humidity_sens))  # Ensure humidity_sens stays within 0 to 100
+        humidity_sens = round(humidity_sens * 2) / 2  # Round to nearest 0.5
+        self.previous_humidity = humidity_sens
+        return humidity_sens, print(f"Humidity: {humidity_sens:.1f}%")
