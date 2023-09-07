3ff3d4897e2999c2ed84ea60820e43882aa2fcf6
Saeid Zolfaghari <saeidz70@gmail.com>
2023-09-07 18:33:23 +0200

    making catalog cleaner

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -2,47 +2,49 @@ import json
 import cherrypy
 
 
-class Configuration(object):
+class Configuration:
     def __init__(self, input_file="catalog.json"):
-        self.services = None
-        self.rest = None
+        self.valid_IDs = []
+        self.valid_types = []
+        self.load_configuration(input_file)
+
+    def load_configuration(self, input_file):
         with open(input_file, "r") as json_file:
             self.content = json.load(json_file)
-            self.services = self.content["services"]
-            self.valid_IDs = []
-            self.valid_types = []
+            self.services = self.content.get("services", {})
+
+        self.service_reader()
 
     def service_reader(self):
-        for service in self.services:
-            self.valid_types.append(service)
-            print("this is the service", service)
-            for ID in self.services[service]:
-                self.valid_IDs.append(ID["service_id"])
+        for service_type, service_data in self.services.items():
+            self.valid_types.append(service_type)
+            print("Service:", service_type)
+            for item in service_data:
+                self.valid_IDs.append(item.get("service_id", ""))
         return self.valid_IDs, self.valid_types
 
     def service_validator(self, *services):
         error_id = "Invalid service ID"
         error_service = "Invalid service type"
         error_request = "Bad request"
+
         self.service_reader()
         services = list(services[0])
         service_len = len(services)
+
         if service_len in range(1, 3):
             if services[0] in self.valid_types:
-                valid_st = self.services[services[0]]
+                valid_st = self.services.get(services[0], [])
                 if service_len == 1:
                     return valid_st
                 elif service_len == 2:
                     if services[1] in self.valid_IDs:
-                        for i in range(len(valid_st)):
-                            if valid_st[i]["service_id"] == services[1]:
-                                return valid_st[i]
-                    else:
+                        for item in valid_st:
+                            if item.get("service_id") == services[1]:
+                                return item
                         return error_id
-            else:
-                return error_service
-        else:
-            return error_request
+            return error_service
+        return error_request
 
     def get_services(self, service_type, service_id):
         self.service_reader()
@@ -51,28 +53,29 @@ class Configuration(object):
 
 
 @cherrypy.expose
-class WebServices(object):
+class WebServices:
     def __init__(self):
         self.config = Configuration()
 
-        pass
-
     def GET(self, *uri):
-        error = "Bad Request"
-        if uri[0] == "services":
+        if not uri:
+            return "Welcome to the Web Service! Use /services to retrieve data."
+
+        if uri and uri[0] == "services":
             if len(uri) == 1:
                 return json.dumps(self.config.services)
             elif len(uri) >= 2:
                 return json.dumps(self.config.service_validator(uri[1:]))
-        else:
-            return error
+
+        return "Invalid URL. Use /services to retrieve data."
 
     def POST(self):
         return True
 
-    def response_json(self, result, success):
-        tempJson = {"result": result, "success": success}
-        return json.dumps(tempJson)
+    @staticmethod
+    def response_json(result, success):
+        temp_json = {"result": result, "success": success}
+        return json.dumps(temp_json)
 
 
 if __name__ == '__main__':
